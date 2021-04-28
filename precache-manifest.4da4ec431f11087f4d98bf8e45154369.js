fbe83589a5f7cb55bc83bda2bb71c5ba74d8fd18
wo_njh <showgur3000@gmail.com>
2021-04-28 10:19:43 +0900

    Updates

---
+++ precache-manifest.4da4ec431f11087f4d98bf8e45154369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c242d48229a08493e39c012be6f627d0",
+    "url": "/basic-react-project/index.html"
+  },
+  {
+    "revision": "90b65f891f450bf4acb0",
+    "url": "/basic-react-project/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/basic-react-project/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/basic-react-project/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b65f891f450bf4acb0",
+    "url": "/basic-react-project/static/js/main.0f63969b.chunk.js"
+  },
+  {
+    "revision": "0efb53789f746a9f498d",
+    "url": "/basic-react-project/static/js/runtime-main.23b7ea97.js"
+  }
+]);
